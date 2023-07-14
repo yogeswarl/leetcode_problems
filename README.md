67cbd33d89632ef8baed80e3244cd19cacd3489e
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-07-14 04:12:13 -0500

    Remove duplicates from sorted array II solution

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetcode_problems
 A repository, where I practice and upload leet code problems that I solve every day
+
+I use python primarily, but I also use Javascript sometimes for problem to submit dual submissions.
---
+++ Remove_Duplicates_from_Sorted_array_11.py
@@ -0,0 +1,24 @@
+# Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+# Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+# Return k after placing the final result in the first k slots of nums.
+
+# Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+# Input: nums = [0,0,1,1,1,1,2,3,3]
+# Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+# Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+# It does not matter what you leave beyond the returned k (hence they are underscores).
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        for num in range(len(nums)-2,0,-1):  
+            if (nums[num]==nums[num-1] and nums[num]==nums[num+1]):
+                nums.pop(num+1)
+        return len(nums)
+    
+sd = Solution() 
+
+print(sd.removeDuplicates([0,0,1,1,1,1,2,3,3]))
