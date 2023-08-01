5bffa9a3dcd0efdf5b39d6ab58b451ed3eca90c7
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-08-01 10:12:10 -0400

    maximum events

--- Two_Sum.py
+++ Two_Sum.py
@@ -12,7 +12,7 @@
 
 # Input: nums = [3,2,4], target = 6
 # Output: [1,2]
-
+from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
