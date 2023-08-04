38f7a9fbf705b2795e214cc34cdbe55dd8448a74
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-08-04 18:40:52 -0400

    day 1 of 30 days of javascript

---
+++ 30DaysofJavascript/HelloWorld.js
@@ -0,0 +1,25 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+/* example
+Input: args = []
+Output: "Hello World"
+Explanation:
+const f = createHelloWorld();
+f(); // "Hello World"
+
+The function returned by createHelloWorld should always return "Hello World".
+*/
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return 'Hello World'
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
