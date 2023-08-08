200192798dd1f1f1464355884945368df3ae655d
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-08-08 17:01:53 -0400

    day 5 of 30 javascript

---
+++ 30DaysofJavascript/Apply_Transform_Over_Each_Element_in_Array.js
@@ -0,0 +1,16 @@
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+  const newArr = [];
+  for (let i =0; i < arr.length;i++)
+      newArr[i] = fn(arr[i],i);
+  return newArr
+};
--- 30DaysofJavascript/Counter.js
+++ 30DaysofJavascript/Counter.js
@@ -21,9 +21,8 @@ var createCounter = function(n) {
   };
 };
 
-/** 
-* const counter = createCounter(10)
-* counter() // 10
-* counter() // 11
-* counter() // 12
-*/
+
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
