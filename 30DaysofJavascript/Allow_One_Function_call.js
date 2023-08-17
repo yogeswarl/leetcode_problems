807a29c17317f7058f33fe3c0f15653a09cd58d3
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-08-17 08:22:21 -0400

    Day 9 of 30 Javascript

---
+++ 30DaysofJavascript/Allow_One_Function_call.js
@@ -0,0 +1,29 @@
+// Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+  let executed = false;
+  return function(...args){
+      if (!executed){  
+          result = fn(...args)
+          executed = true
+          return result}
+      else{
+          return undefined}
+
+  }
+};
+
+/**
+* let fn = (a,b,c) => (a + b + c)
+* let onceFn = once(fn)
+*
+* onceFn(1,2,3); // 6
+* onceFn(2,3,6); // returns undefined without calling fn
+*/
