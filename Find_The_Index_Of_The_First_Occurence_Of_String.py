30d6a89d1dd167ec04c70e6c247cadd04d5b4c60
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-08-17 08:07:58 -0400

    find the index of the first occurence :solution

---
+++ Find_The_Index_Of_The_First_Occurence_Of_String.py
@@ -0,0 +1,8 @@
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+sd = Solution()
+print(sd.strStr("sadbutsad","sad"))
+print(sd.strStr("leetcode","leeto"))
