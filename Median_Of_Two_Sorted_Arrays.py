2cd1260399d41ba82ecf84e4a78b873a34d0b271
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2023-08-22 15:18:48 -0400

    add object and test for solution

--- Median_Of_Two_Sorted_Arrays.py
+++ Median_Of_Two_Sorted_Arrays.py
@@ -11,8 +11,13 @@
 # Output: 2.50000
 # Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 import statistics
+from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         list3 = nums1 + nums2
         list3 = sorted(list3)
         return statistics.median(list3)
+    
+sd = Solution()
+sd.findMedianSortedArrays([1,3],[2]) # 2.00000
+
