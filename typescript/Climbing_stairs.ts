c6c544095fb045a3ff620920e8100d2151788c24
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2024-08-05 09:46:58 -0400

    Climbing Stairs Update

--- typescript/Climbing_stairs.ts
+++ typescript/Climbing_stairs.ts
@@ -1,7 +1,19 @@
 // Time complexity issues
+
+// function climbStairs(n: number): number {
+//   if( n < 4){
+//       return n 
+//   }
+//   return climbStairs(n - 1) + climbStairs(n - 2)
+// };
+
 function climbStairs(n: number): number {
-  if( n < 4){
-      return n 
+  if (n <= 1) return 1;
+  let firstNum: number = 1, secondNum: number = 1, thirdNum: number = 0;
+  for (let i: number = 2; i <= n; i++) {
+      thirdNum = firstNum + secondNum;
+      firstNum = secondNum;
+      secondNum = thirdNum;
   }
-  return climbStairs(n - 1) + climbStairs(n - 2)
-};
+  return thirdNum;
+}
