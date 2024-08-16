8a5e5e2c1fe69f05e6685ec6b983c866f25454e4
Yogeswar Lakshmi Narayanan <yogi2198@gmail.com>
2024-08-16 00:57:33 -0400

    Maximum Distance of Arrays+

---
+++ typescript/Maximum_Distance_Of_Arrays.ts
@@ -0,0 +1,44 @@
+function maxDistance(arrays: number[][]): number {
+  let max = -Infinity
+  let maxI = -1
+  let min = Infinity
+  let minI = -1
+
+  for (let i = 0; i < arrays.length; i++) {
+      const arr = arrays[i]
+      if (arr[0] < min ) {
+          minI = i
+          min = arr[0]
+      }
+
+      if (arr[arr.length - 1] > max ) {
+          maxI = i
+          max = arr[arr.length - 1]
+      }    
+  }
+
+  let maxBasedDelta = 0
+  let minBasedDelta = 0
+
+  if (minI != maxI) {
+      return Math.abs( max - min )
+  }
+
+  for (let i = 0; i < arrays.length; i++ ) {
+      const arr = arrays[i]
+
+      if (i !=  maxI) {
+          let val = Math.abs( max -  arr[0])
+          maxBasedDelta = Math.max(val, maxBasedDelta)
+      }
+
+      if (i !=  minI) {
+          let val = Math.abs( arr[arr.length - 1] -  min)
+          minBasedDelta = Math.max(val, minBasedDelta)
+      } 
+
+  }
+
+
+  return Math.max(minBasedDelta, maxBasedDelta)
+};
